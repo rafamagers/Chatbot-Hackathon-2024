5d9f24bfeecca03c404fdadab8b7c512e2adf7c4
estudiante <estudiante@CampusUninorte.local>
2024-07-27 13:37:52 -0500

    Interfaz y conexion chat bot

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCvjwgRFDJeUNZac-H8rOiX7ffZNiQCooU',
+    authDomain: 'chat-bot-con-gemini.firebaseapp.com',
+    projectId: 'chat-bot-con-gemini',
+    storageBucket: 'chat-bot-con-gemini.appspot.com',
+    messagingSenderId: '150289189968',
+    appId: '1:150289189968:web:17e71dd91c8e847021d555',
+  },
+};
